5333c9621a02fa1d1cf5724a5fd7bd0661f818d7
OctKun <kunk97@163.com>
2026-04-06 16:53:51 +0800

    chore: 删除项目文件及配置

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import HomePageClient from './HomePageClient';
-import { getRecentPosts } from '@/lib/blog.server';
-
-export default function Home() {
-  const recentPosts = getRecentPosts(3);
-
-  return <HomePageClient recentPosts={recentPosts} />;
-}
